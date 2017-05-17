959c687cbc1b12e6ab20ca2539310da0a2a0bebe
Kawachi <kawachi.shusaku@opensesame-tech.com>
2017-05-17 10:56:55 +0900

    Emotion APIコール

--- LineBotCompanyTrip/LineBotCompanyTrip/Services/LineBot/LineBotService.cs
+++ LineBotCompanyTrip/LineBotCompanyTrip/Services/LineBot/LineBotService.cs
@@ -1,5 +1,6 @@
 ﻿
 using LineBotCompanyTrip.Configurations;
+using LineBotCompanyTrip.Services.Emotion;
 using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Net.Http;
@@ -140,35 +141,50 @@ namespace LineBotCompanyTrip.Services.LineBot {
 		/// <returns></returns>
 		public async Task CallImageMessageEvent( string replyToken , string messageId ) {
 
-			string binaryImage = await this.GetContent( messageId );
-			Trace.TraceInformation( "Binary Image is : " + binaryImage );
+			#region 画像のバイナリデータをサーバに保存する
+			string imagePath = "https://manuke.jp/wp-content/uploads/2016/05/chomado2.jpg";
+			{
+				string binaryImage = await this.GetContent( messageId );
+				Trace.TraceInformation( "Binary Image is : " + binaryImage );
+			}
+			#endregion
 
-			RequestOfReplyMessage requestObject = new RequestOfReplyMessage();
-			requestObject.replyToken = replyToken;
-			RequestOfReplyMessage.Message message = new RequestOfReplyMessage.Message();
-			message.type = "text";
-			message.text = "画像が送られてきました！";
-			requestObject.messages = new RequestOfReplyMessage.Message[ 1 ];
-			requestObject.messages[ 0 ] = message;
+			#region Emotion APIより画像を解析する
+			string emotionResult = await new EmotionService().Call( imagePath );
+			#endregion
 
-			string jsonRequest = JsonConvert.SerializeObject( requestObject );
-			StringContent content = new StringContent( jsonRequest );
-			content.Headers.ContentType = new MediaTypeHeaderValue( "application/json" );
+			#region 解析結果を通知する
+			{
 
-			HttpClient client = new HttpClient();
-			client.DefaultRequestHeaders.Accept.Add( new MediaTypeWithQualityHeaderValue( "application/json" ) );
-			client.DefaultRequestHeaders.Add( "Authorization" , "Bearer {" + LineBotConfig.ChannelAccessToken + "}" );
+				RequestOfReplyMessage requestObject = new RequestOfReplyMessage();
+				requestObject.replyToken = replyToken;
+				RequestOfReplyMessage.Message message = new RequestOfReplyMessage.Message();
+				message.type = "text";
+				message.text = "画像が送られてきました\n" + emotionResult;
+				requestObject.messages = new RequestOfReplyMessage.Message[ 1 ];
+				requestObject.messages[ 0 ] = message;
 
-			HttpResponseMessage response = await client.PostAsync( LineBotConfig.ReplyMessageUrl , content ).ConfigureAwait( false );
-			string result = await response.Content.ReadAsStringAsync().ConfigureAwait( false );
+				string jsonRequest = JsonConvert.SerializeObject( requestObject );
+				StringContent content = new StringContent( jsonRequest );
+				content.Headers.ContentType = new MediaTypeHeaderValue( "application/json" );
+
+				HttpClient client = new HttpClient();
+				client.DefaultRequestHeaders.Accept.Add( new MediaTypeWithQualityHeaderValue( "application/json" ) );
+				client.DefaultRequestHeaders.Add( "Authorization" , "Bearer {" + LineBotConfig.ChannelAccessToken + "}" );
+
+				HttpResponseMessage response = await client.PostAsync( LineBotConfig.ReplyMessageUrl , content ).ConfigureAwait( false );
+				string result = await response.Content.ReadAsStringAsync().ConfigureAwait( false );
+
+			}
+			#endregion
 
 		}
 
 		/// <summary>
-		/// Contentから画像、動画、音声にアクセスするAPIを呼ぶ
+		/// Contentから画像、動画、音声にアクセスするAPIを呼び、バイナリデータを返す
 		/// </summary>
 		/// <param name="messageId">メッセージID</param>
-		/// <returns>結果</returns>
+		/// <returns>バイナリデータ</returns>
 		private async Task<string> GetContent( string messageId ) {
 
 			HttpClient client = new HttpClient();
